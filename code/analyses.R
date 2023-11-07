836a5a625ddb0827c069201470448eb8d7b0944d
bernibra <bernat.bramon@gmail.com>
2023-11-07 14:25:57 +0100

    Adding files for tutorial

--- code/analyses.R
+++ code/analyses.R
@@ -2,7 +2,7 @@ library(dplyr)
 source("./code/utility.R")
 
 data <- readRDS("./data/processed/data.rds")
-correlation <- cor(data$films, data$deaths,  method = "spearman")
+correlation <- cor(data$films, data$deaths,  method = "pearson")
 
 make_time(4, "Calculate correlation")
 
--- code/process_data.R
+++ code/process_data.R
@@ -10,3 +10,5 @@ data <- cage %>% left_join(drowning, by = "year")
 make_time(4, "processing data")
 
 saveRDS(object = data, file = "./data/processed/data.rds")
+
+
